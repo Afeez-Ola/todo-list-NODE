5c74e1ca86ddc52ead5d6faa2098a2c35c01f700
Afeez-Ola <afeezbolajiola@gmail.com>
2021-03-02 20:36:34 +0100

    🎨 IMPROVE:

--- app.js
+++ app.js
@@ -1,69 +1,40 @@
 // jshint esversion:6
 
-const express = require('express');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 const app = express();
 
-
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
 // const items = ['Drink Coffee', 'Buy fye', 'Do Dishes'];
 // const workItems = [];
 
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-mongoose.connect(
-    'mongodb://localhost:27017/todo-listDB', {
-        useNewUrlParser: true,
-        useCreateIndex: true,
-        useUnifiedTopology: true
-    });
+mongoose.connect("mongodb://localhost:27017/todo-listDB", {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+});
 
 const itemSchema = {
-    name: String
+    name: String,
 };
 
-const Item = mongoose.model('Item', itemSchema)
+const Item = mongoose.model("Item", itemSchema);
 
-const item1 = new Item({
-    name: 'Eating Sand'
-})
-const item2 = new Item({
-    name: 'Playing PES'
-})
-const item3 = new Item({
-    name: 'Praying '
-})
-
-const defaultItem = [item1, item2, item3];
-
-Item.insertMany(defaultItem, function(err) {
-    if (err) {
-        console.log(err);
-    } else {
-
-        mongoose.connection.close();
-        console.log('SUCCESS!');
-    }
-})
-
-Item.find({}, (err, result) => {
-    // console.log(result);
-    console.log('====================================');
-    console.log(result);
-    console.log('====================================');
-})
-
-app.get('/', function(req, res) {
+app.get("/", function(req, res) {
     Item.find({}, (err, result) => {
         // if (err) { console.log(err) }
         console.log(err);
-        console.log(result)
-    })
-})
+        console.log(result);
+    });
+});
+
+
 
 
 
@@ -111,7 +82,6 @@ app.get('/about', (req, res) => {
 
 
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`We are live at port ${PORT}`)
-})
+app.listen(process.env.PORT || 3001, function() {
+    console.log(`Local: http://localhost:3000`);
+});
